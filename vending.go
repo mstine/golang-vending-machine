8181fba19c92e38396fc6b21740e0f05052d53e7
Matt Stine <mstine@gopivotal.com>
2014-01-18 00:27:24 -0600

    initial commit

---
+++ vending.go
@@ -0,0 +1,90 @@
+package vending
+
+import "container/list"
+
+type Item struct {
+	price int
+	count int
+}
+
+type Coin struct {
+	value int
+	label string
+}
+
+type VendingMachine struct {
+	items map[string]*Item
+	coins map[string]*Coin
+	coinsInserted *list.List
+}
+
+type VendingMachineError struct {
+	Message string
+}
+
+func NewVendingMachine() *VendingMachine {
+	v := new(VendingMachine)
+
+	v.items = make(map[string]*Item)
+	v.items["A"] = &Item{65,0}
+	v.items["B"] = &Item{100,0}
+	v.items["C"] = &Item{150,0}
+
+	v.coins = make(map[string]*Coin)
+	v.coins["N"] = &Coin{5,"N"}
+	v.coins["D"] = &Coin{10,"D"}
+	v.coins["Q"] = &Coin{25,"Q"}
+	v.coins["DD"] = &Coin{100,"DD"}
+
+	v.coinsInserted = list.New()
+
+	return v
+}
+
+func (e *VendingMachineError) Error() string {
+	return e.Message
+}
+
+func (v *VendingMachine) Get(item string) (string, error) {
+	if v.items[item].count > 0 {
+		v.items[item].count--
+		return item, nil
+	} else {
+		return "", &VendingMachineError{"No " + item + " items available!"}
+	}
+}
+
+func (v *VendingMachine) Service() {
+	v.items["A"].count = 50
+	v.items["B"].count = 50
+	v.items["C"].count = 50	
+}
+
+func (v *VendingMachine) CoinReturn() (string, error) {
+	e := v.coinsInserted.Front();
+	coinReturn := "";
+	if e != nil {
+		coinReturn += e.Value.(*Coin).label
+		for e != nil {
+			e = e.Next()
+			if e != nil {
+				coinReturn += ", "	
+				coinReturn += e.Value.(*Coin).label	
+			}
+		}		
+		v.coinsInserted = list.New()
+	}
+	return coinReturn, nil
+}
+
+func (v *VendingMachine) Insert(coin string) {
+	v.coinsInserted.PushBack(v.coins[coin])
+}
+
+func (v *VendingMachine) AmountInserted() int {
+	amount := 0
+	for e := v.coinsInserted.Front(); e != nil; e = e.Next() {
+		amount += e.Value.(*Coin).value
+	}
+	return amount
+}
---
+++ vending_test.go
@@ -0,0 +1,148 @@
+package vending
+
+import "testing"
+
+func TestBuyFromAEmptyMachine(t *testing.T) {
+	v := NewVendingMachine()
+	result, error := v.Get("A")
+	
+	if result == "A" {
+		t.Errorf("Empty machine should not return an A!")
+	}
+
+	if error.Error() != "No A items available!" {
+		t.Errorf("Wrong error message!")
+	}
+}
+
+func TestBuyFromBEmptyMachine(t *testing.T) {
+	v := NewVendingMachine()
+	result, error := v.Get("B")
+	
+	if result == "B" {
+		t.Errorf("Empty machine should not return a B")
+	}
+
+	if error.Error() != "No B items available!" {
+		t.Errorf("Wrong error message!")
+	}
+}
+
+func TestBuyFromCEmptyMachine(t *testing.T) {
+	v := NewVendingMachine()
+	result, error := v.Get("C")
+	
+	if result == "C" {
+		t.Errorf("Empty machine should not return a C")
+	}
+
+	if error.Error() != "No C items available!" {
+		t.Errorf("Wrong error message!")
+	}
+}
+
+func TestServiceMachine(t *testing.T) {
+	v := NewVendingMachine()
+	v.Service()
+
+	if v.items["A"].count != 50 || v.items["B"].count != 50 || v.items["C"].count != 50 {
+		t.Errorf("Machine was not properly serviced!")
+	}
+}
+
+func TestBuyAFromServicedMachine(t *testing.T) {
+	v := NewVendingMachine()
+	v.Service()
+	result, _ := v.Get("A")
+	if result != "A" {
+		t.Errorf("Machine should have returned an A!")
+	}
+	if v.items["A"].count != 49 {
+		t.Errorf("Machine did not properly decrement inventory!")
+	}
+}
+
+func TestBuyBFromServicedMachine(t *testing.T) {
+	v := NewVendingMachine()
+	v.Service()
+	result, _ := v.Get("B")
+	if result != "B" {
+		t.Errorf("Machine should have returned a B!")
+	}
+	if v.items["B"].count != 49 {
+		t.Errorf("Machine did not properly decrement inventory!")
+	}
+}
+
+func TestBuyCFromServicedMachine(t *testing.T) {
+	v := NewVendingMachine()
+	v.Service()
+	result, _ := v.Get("C")
+	if result != "C" {
+		t.Errorf("Machine should have returned a C!")
+	}
+	if v.items["C"].count != 49 {
+		t.Errorf("Machine did not properly decrement inventory!")
+	}
+}
+
+func TestEmptyCoinReturn(t *testing.T) {
+	v := NewVendingMachine()
+	result, _ := v.CoinReturn()
+	if result != "" {
+		t.Errorf("CoinReturn should be empty string with no money inserted!")
+	}
+}
+
+func TestInsertNickel(t *testing.T) {
+	v := NewVendingMachine()
+	v.Insert("N")
+	if v.AmountInserted() != 5 {
+		t.Errorf("Inserting a single nickle should result in 5 cents total inserted!")
+	}
+}
+
+func TestCoinReturnWithSingleNickle(t *testing.T) {
+	v := NewVendingMachine()
+	v.Insert("N")
+	result, _ := v.CoinReturn();
+	if result != "N" {
+		t.Errorf("CoinReturn should be 'N' after inserting single nickle!")
+	}
+}
+
+func TestTwoNickles(t *testing.T) {
+	v := NewVendingMachine()
+	v.Insert("N")
+	v.Insert("N")
+
+	if v.AmountInserted() != 10 {
+		t.Errorf("Inserting two nickles should result in 10 cents total inserted!")
+	}
+
+	result, _ := v.CoinReturn();
+
+	if result != "N, N" {
+		t.Errorf("CoinReturn should be 'N, N' after inserting two nickles. Was: '" + result + "'");
+	}
+}
+
+func TestSomeCoins(t *testing.T) {
+	v := NewVendingMachine()
+	v.Insert("N")
+	v.Insert("Q")
+	v.Insert("DD")
+	v.Insert("D")
+	v.Insert("N")
+	v.Insert("D")
+
+	if v.AmountInserted() != 155 {
+		t.Errorf("Amoint should be 155 cents!")
+	}
+
+	result, _ := v.CoinReturn();
+
+	if result != "N, Q, DD, D, N, D" {
+		t.Errorf("CoinReturn should be 'N, Q, DD, D, N, D', but was: '" + result +"'");
+	}
+}
